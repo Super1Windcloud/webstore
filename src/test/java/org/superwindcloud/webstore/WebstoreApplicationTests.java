b138ee3fbc3bbf1528416635e0290118e2bcb15a
SuperWindCloud <19338823723@163.com>
2026-04-03 23:13:39 +0800

    Add Maven wrapper, project structure, and initial configuration

---
+++ src/test/java/org/superwindcloud/webstore/WebstoreApplicationTests.java
@@ -0,0 +1,11 @@
+package org.superwindcloud.webstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstoreApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
