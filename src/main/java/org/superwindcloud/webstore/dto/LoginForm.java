7c9b619d267c9da5c33472d82e15355e78e77d5b
SuperWindCloud <19338823723@163.com>
2026-04-04 20:06:34 +0800

    Update status pill styling and add new CSS variables for better visual feedback
    Signed-off-by: SuperWind
    Signed-off-by: SuperWindCloud <19338823723@163.com>

--- src/main/java/org/superwindcloud/webstore/dto/LoginForm.java
+++ src/main/java/org/superwindcloud/webstore/dto/LoginForm.java
@@ -1,7 +1,11 @@
 package org.superwindcloud.webstore.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class LoginForm {
 
   @NotBlank(message = "Username is required")
@@ -10,19 +14,4 @@ public class LoginForm {
   @NotBlank(message = "Password is required")
   private String password;
 
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
 }
--- src/main/java/org/superwindcloud/webstore/model/SystemOverview.java
+++ src/main/java/org/superwindcloud/webstore/model/SystemOverview.java
@@ -1,6 +1,6 @@
 package org.superwindcloud.webstore.model;
 
-public record SystemOverview(
+public  record SystemOverview(
     Metric diskSpace, Metric cpuLoad, Metric memoryUsage, String hostName) {
 
   public record Metric(String title, String primaryValue, String secondaryValue) {}
